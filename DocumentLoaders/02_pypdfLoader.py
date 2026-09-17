950f0b10f136380090484cc3bfef52afa639019e
Amandeep Singh <amandeep87557@gmail.com>
2026-09-17 20:54:21 +0530

    PyPDFLoader implementation

---
+++ DocumentLoaders/02_pypdfLoader.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader(r'E:\Generative AI\Files\sample_3_to_4_page_text.pdf')
+
+docs = loader.load()
+
+print(len(docs)) # total number of pages
+print(docs[0].page_content)
+print(docs[0].metadata)
