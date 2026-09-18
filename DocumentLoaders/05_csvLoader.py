82f34e596f6a55cefea7caecb36a314dfb229992
Amandeep Singh <amandeep87557@gmail.com>
2026-09-18 10:10:44 +0530

    CSVLoader implementation

---
+++ DocumentLoaders/05_csvLoader.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from langchain_community.document_loaders import CSVLoader
+
+document = CSVLoader(r'E:\Generative AI\Files\Position_Salaries.csv')
+
+loader = document.load()
+
+print(loader[0].page_content)
+print(loader[1].page_content)
